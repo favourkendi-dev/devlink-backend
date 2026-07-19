6c3fa41dff904ed1caff33f16ae8345dac304d81
favourkendi-dev <favour.kendi@student.moringaschool.com>
2026-07-19 20:44:45 +0300

    auto create profile when a new user registers using django signals

--- accounts/apps.py
+++ accounts/apps.py
@@ -2,4 +2,10 @@ from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
-    name = "accounts"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'accounts'
+
+    # This runs when the accounts app is ready We import signals.py here so Django actually registers our create_profile function to listen for the post_save event
+    
+    def ready(self):
+        import accounts.signals
